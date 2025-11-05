b7d21e86eba915c1c23bf8fe7d6262c50094f494
tjlcast <phx108@sina.com>
2025-11-05 19:50:44 +0800

    refactor: simplify write_to_file XML structure and update path description

--- prompt_toolkit_demo/src/examples/ai_chat_modular/tools/write_to_file/description.py
+++ prompt_toolkit_demo/src/examples/ai_chat_modular/tools/write_to_file/description.py
@@ -1,32 +1,23 @@
 desc = """
 ## write_to_file
 Description: Request to write content to a file. This tool is primarily used for **creating new files** or for scenarios where a **complete rewrite of an existing file is intentionally required**. If the file exists, it will be overwritten. If it doesn't exist, it will be created. This tool will automatically create any directories needed to write the file.
-
 Parameters:
-- args: Contains one or more file elements, where each file contains:
-  - path: (required) The path of the file to write to (relative to the current workspace directory)
-  - content: (required) The content to write to the file. When performing a full rewrite of an existing file or creating a new one, ALWAYS provide the COMPLETE intended content of the file, without any truncation or omissions. You MUST include ALL parts of the file, even if they haven't been modified. Do NOT include the line numbers in the content though, just the actual content of the file.
-  - line_count: (required) The number of lines in the file. Make sure to compute this based on the actual content of the file, not the number of lines in the content you're providing.
-
+- path: (required) The path of the file to write to (relative to the current workspace directory C:\Users\phx10\code\cg-manager)
+- content: (required) The content to write to the file. When performing a full rewrite of an existing file or creating a new one, ALWAYS provide the COMPLETE intended content of the file, without any truncation or omissions. You MUST include ALL parts of the file, even if they haven't been modified. Do NOT include the line numbers in the content though, just the actual content of the file.
+- line_count: (required) The number of lines in the file. Make sure to compute this based on the actual content of the file, not the number of lines in the content you're providing.
 Usage:
 <write_to_file>
-<args>
-  <file>
-    <path>File path here</path>
-    <content>
+<path>File path here</path>
+<content>
 Your file content here
 </content>
-    <line_count>total number of lines in the file, including empty lines</line_count>
-  </file>
-</args>
+<line_count>total number of lines in the file, including empty lines</line_count>
 </write_to_file>
 
 Example: Requesting to write to frontend-config.json
 <write_to_file>
-<args>
-  <file>
-    <path>frontend-config.json</path>
-    <content>
+<path>frontend-config.json</path>
+<content>
 {
   "apiEndpoint": "https://api.example.com",
   "theme": {
@@ -42,8 +33,6 @@ Example: Requesting to write to frontend-config.json
   "version": "1.0.0"
 }
 </content>
-    <line_count>14</line_count>
-  </file>
-</args>
+<line_count>14</line_count>
 </write_to_file>
 """
